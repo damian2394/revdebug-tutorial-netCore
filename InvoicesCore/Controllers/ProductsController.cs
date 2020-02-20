7915210481ea3928157976b450c7b119204acda3
Damian Tasevski <damian@revdebug.com>
2020-02-20 12:34:54 +0100

    Added products endpoint

---
+++ InvoicesCore/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Invoices.DataAccess;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace InvoicesCore.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public ProductsController()
+        {
+            ReconcileData.Init();
+        }
+
+        public async Task<string> All()
+        {
+            var products = ReconcileData.Products;
+            var productsJson = JsonConvert.SerializeObject(products);
+            return productsJson;
+        }
+    }
+}
